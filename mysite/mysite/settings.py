a8785969fa12259f68dc4c32418cb12c6658246d
Lee Kadin <lkadin@gmail.com>
2023-03-14 12:02:51 -0400

    remove commented code

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -30,14 +30,12 @@ ALLOWED_HOSTS = ["*"]
 # Application definition
 
 INSTALLED_APPS = [
-    # 'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # 'catalog.apps.CatalogConfig',
     'coup.apps.CoupConfig',
 ]
 
