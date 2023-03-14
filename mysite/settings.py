3d1ce2cd419c0c173e13c039570eaf543707a7a1
Lee Kadin <lkadin@gmail.com>
2023-03-14 13:17:43 -0400

    Change debug default to False

--- mysite/settings.py
+++ mysite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
-DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
+DEBUG = bool(os.environ.get('DJANGO_DEBUG', False))
 ALLOWED_HOSTS = ["*"]
 
 # Application definition
